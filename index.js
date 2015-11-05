9e336865eacea9819cb51e74600bff540dd1a429
Seiei Miyagi <hanachin@gmail.com>
2015-11-05 17:59:48 +0900

    Add initial index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+exports.install = function (Vue, opts) {
+  Vue.filter('enterKey', function (value, char) {
+    char = char || '↵';
+    return value.replace(/\n/, '\n' + char);
+  });
+}
